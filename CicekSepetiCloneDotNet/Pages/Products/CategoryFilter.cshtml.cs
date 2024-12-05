8bfccaf236b12f90bc4ec5eb86f7fd9c53df69af
AhmetCanArslan <107195095+AhmetCanArslan@users.noreply.github.com>
2024-12-05 23:21:27 +0300

    Added Category Filter

---
+++ CicekSepetiCloneDotNet/Pages/Products/CategoryFilter.cshtml.cs
@@ -0,0 +1,55 @@
+using CicekSepetiCloneDotNet.Pages.Index;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace CicekSepetiCloneDotNet.Pages.Products
+{
+    public class CategoryFilterModel : PageModel
+    {
+        string errorMessage = "";
+        public List<ProductInfo> productList = new List<ProductInfo>();
+        public void OnGet()
+        {
+            String id = Request.Query["id"];
+            
+
+
+            try
+            {
+                String connectionString = "Data Source=JUANWIN\\SQLEXPRESS;Initial Catalog=DbProjectCicekSepeti;Integrated Security=True;Encrypt=False";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "Select * from TBL_PRODUCTS where product_category_id = @id";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ProductInfo productInfo = new ProductInfo();
+                                productInfo.product_id = "" + reader.GetInt32(0);
+                                productInfo.product_name = reader.GetString(1);
+                                productInfo.product_description = reader.GetString(2);
+                                productInfo.product_price = "" + reader.GetInt32(3);
+                                productInfo.product_image = reader.GetString(4);
+                                productInfo.product_categoryid = "" + reader.GetInt32(5);
+
+                                productList.Add(productInfo);
+
+                            }
+
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+    }
+}
---
+++ CicekSepetiCloneDotNet/Pages/Shared/DefaultPageLayout.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using CicekSepetiCloneDotNet.Pages.Categories;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace CicekSepetiCloneDotNet.Pages.Shared
+{
+    public class DefaultPageLayout : PageModel
+    {
+        public Dictionary<int, List<CategoryInfo>> CategoriesByParentId { get; set; } = new Dictionary<int, List<CategoryInfo>>();
+
+        public void LoadCategories()
+        {
+            try
+            {
+                string connectionString = "Data Source=JUANWIN\\SQLEXPRESS;Initial Catalog=DbProjectCicekSepeti;Integrated Security=True;Encrypt=False";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT category_id, category_name, category_parent_id FROM TBL_Category";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var category = new CategoryInfo
+                                {
+                                    category_id = ""+reader.GetInt32(0),
+                                    category_name = reader.GetString(1),
+                                    category_parent_id =""+ reader.GetInt32(2) 
+                                };
+
+                                int parentId =  Convert.ToInt32(category.category_parent_id);
+
+                                if (!CategoriesByParentId.ContainsKey(parentId))
+                                {
+                                    CategoriesByParentId[parentId] = new List<CategoryInfo>();
+                                }
+
+                                CategoriesByParentId[parentId].Add(category);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
