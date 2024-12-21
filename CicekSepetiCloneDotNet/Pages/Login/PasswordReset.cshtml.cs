4f6e16cd1ab9f50f7f086008706dcdc35bc24456
AhmetCanArslan <107195095+AhmetCanArslan@users.noreply.github.com>
2024-12-21 16:03:31 +0300

    password resetting is completed

--- CicekSepetiCloneDotNet/Pages/Login/PasswordReset.cshtml.cs
+++ CicekSepetiCloneDotNet/Pages/Login/PasswordReset.cshtml.cs
@@ -1,12 +1,55 @@
+﻿using CicekSepetiCloneDotNet.Pages.AdminPage.Users;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Data.SqlClient;
 
 namespace CicekSepetiCloneDotNet.Pages.Login
 {
     public class PasswordResetModel : PageModel
     {
+        public string connectionString = "Data Source=JUANWIN\\SQLEXPRESS;Initial Catalog=DbProjectCicekSepeti;Integrated Security=True;Encrypt=False";
+        public string errorMessage = "";
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            string mail = Request.Form["email"];
+            string phone = Request.Form["phone"];
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM TBL_users where user_mail=@mail and user_number=@phone";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@mail", mail);
+                        command.Parameters.AddWithValue("@phone", phone);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                UsersInfo userInfo = new UsersInfo();
+                                userInfo.user_id = "" + reader.GetInt32(0);
+
+                                Response.Redirect("/Login/PasswordResetAuth?id="+userInfo.user_id);
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                errorMessage = ex.Message;
+                Console.WriteLine(ex.ToString());
+            }
+            errorMessage = "Kullanıcı Bulunamadı";
+            RedirectToPage("/Login/PasswordReset");
         }
     }
 }
---
+++ CicekSepetiCloneDotNet/Pages/Login/PasswordResetAuth.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using CicekSepetiCloneDotNet.Pages.AdminPage.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Data.SqlClient;
+
+namespace CicekSepetiCloneDotNet.Pages.Login
+{
+    public class PasswordResetAuthModel : PageModel
+    {
+        public string errorMessage = "";
+        public string connectionString = "Data Source=JUANWIN\\SQLEXPRESS;Initial Catalog=DbProjectCicekSepeti;Integrated Security=True;Encrypt=False";
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            string id = Request.Query["id"];
+            string password1 = Request.Form["password1"];
+            string password2 = Request.Form["password2"];
+
+            if (password1 != password2)
+            {
+                errorMessage = "Şifreler Uyuşmuyor";
+                Response.Redirect("/Login/PasswordResetAuth");
+            }
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "update tbl_users set user_pass = @password1 where user_id=@id";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@password1", password1);
+                        command.Parameters.AddWithValue("@id", id);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+            Response.Redirect("/Login/Index");
+        }
+    }
+}
